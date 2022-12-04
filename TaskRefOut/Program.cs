bd6ca3735f9fb04f31d740dad7c7ad3239f2a784
Timur Guliev <mail@guliev-timur.ru>
2022-12-04 22:24:20 +0500

    add class

--- TaskRefOut/Program.cs
+++ TaskRefOut/Program.cs
@@ -11,66 +11,82 @@
      и пытается преобразовать в число, если получилось то возвращается true, если нет то false
 */
 
-// Задача №1
-void Changer (ref int i, ref int j) {
-    int temp = i;
-    i = j;
-    j = temp;
-}
-int oneParametr = 1;
-int twoParametr = 2;
-Console.WriteLine("Значения до передачи в метод Changer: oneParametr=" + oneParametr + "; twoParametr=" + twoParametr);
-Changer(ref oneParametr, ref twoParametr);
-Console.WriteLine("Значения после передачи в метод Changer: oneParametr=" + oneParametr + "; twoParametr=" + twoParametr);
+using System;
+public class TaskRefOut
+{
+    public static void Main()
+    {
+
+        // Задача №1
+        void Changer (ref int i, ref int j) {
+            int temp = i;
+            i = j;
+            j = temp;
+        }
+        int oneParametr = 1;
+        int twoParametr = 2;
+        Console.WriteLine("Значения до передачи в метод Changer: oneParametr=" + oneParametr + "; twoParametr=" + twoParametr);
+        Changer(ref oneParametr, ref twoParametr);
+        Console.WriteLine("Значения после передачи в метод Changer: oneParametr=" + oneParametr + "; twoParametr=" + twoParametr);
 
 
-//Задача №2
-bool ParityCheck (int i, out string result) {
-    if (i % 2 == 0){
-        result = (i / 2).ToString();
-        return true; 
-    } else {
-        result = "\"не делится\"";
-        return false;
+        //Задача №2
+        bool ParityCheck (int i, out string result) {
+            if (i % 2 == 0){
+                result = (i / 2).ToString();
+                return true; 
+            } else {
+                result = "\"не делится\"";
+                return false;
+            }
+        }
+        string result = "не делится";
+        Console.WriteLine("2 четное число? " + ParityCheck(2, out result) + " Результат деления на 2 равен " + result);
+        Console.WriteLine("5 четное число? " + ParityCheck(5, out result) + " Результат деления на 2 равен " + result);
+
+        //Задача №3
+
+        bool IntToArray (int i, out int[] res) {
+            try {
+                res = Math.Abs(i).ToString().Select(t=>int.Parse(t.ToString())).ToArray();
+                return true;
+            } catch (Exception e) {
+                res = new int[0];
+                return false;
+            }
+        }
+
+        int[] res;
+        Console.WriteLine(IntToArray(125634, out res) + " Результат: " + res[0]);
+        Console.WriteLine(IntToArray(0, out res) + " Результат: " + res[0]);
+        Console.WriteLine(IntToArray(-1, out res) + " Результат: " + res[0]);
+        Console.WriteLine(IntToArray(-5826, out res) + " Результат: " + res[0]);
+
+        // Задача №4
+        
+
+        int num;
+        Console.WriteLine(MyTryParse("1234567890", out num) + " Результат: " + num);
+        Console.WriteLine(MyTryParse("0", out num) + " Результат: " + num);
+        Console.WriteLine(MyTryParse("-1", out num) + " Результат: " + num);
+        Console.WriteLine(MyTryParse("-5826", out num) + " Результат: " + num);
+        Console.WriteLine(MyTryParse("ouwrhvwre45375367", out num) + " Результат: " + num);
     }
-}
-string result = "не делится";
-Console.WriteLine("2 четное число? " + ParityCheck(2, out result) + " Результат деления на 2 равен " + result);
-Console.WriteLine("5 четное число? " + ParityCheck(5, out result) + " Результат деления на 2 равен " + result);
-
-//Задача №3
-
-bool IntToArray (int i, out int[] res) {
-    try {
-        res = Math.Abs(i).ToString().Select(t=>int.Parse(t.ToString())).ToArray();
-        return true;
-    } catch (Exception e) {
-        res = new int[0];
-        return false;
+    
+    public static bool MyTryParse (string s, out int rslt) {
+        try {
+            rslt = int.Parse(s);
+            return true;
+        } catch (Exception e) {
+            rslt = 0;
+            return false;
+        }
     }
+
 }
 
-int[] res;
-Console.WriteLine(IntToArray(125634, out res) + " Результат: " + res[0]);
-Console.WriteLine(IntToArray(0, out res) + " Результат: " + res[0]);
-Console.WriteLine(IntToArray(-1, out res) + " Результат: " + res[0]);
-Console.WriteLine(IntToArray(-5826, out res) + " Результат: " + res[0]);
 
-//Задача №4
 
-bool MyTryParse (string s, out int rslt) {
-    try {
-        rslt = int.Parse(s);
-        return true;
-    } catch (Exception e) {
-        rslt = 0;
-        return false;
-    }
-}
 
-int num;
-Console.WriteLine(MyTryParse("125634", out num) + " Результат: " + num);
-Console.WriteLine(MyTryParse("0", out num) + " Результат: " + num);
-Console.WriteLine(MyTryParse("-1", out num) + " Результат: " + num);
-Console.WriteLine(MyTryParse("-5826", out num) + " Результат: " + num);
-Console.WriteLine(MyTryParse("ouwrhvwre45375367", out num) + " Результат: " + num);
+
+
