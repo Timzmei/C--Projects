70d674b66f9e5df20c1046896dd725ce0304b775
Timur Guliev <mail@guliev-timur.ru>
2022-12-14 21:57:57 +0500

    add HomeTask005

---
+++ HomeTask005/Program.cs
@@ -0,0 +1,137 @@
+﻿
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// оторая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+// Задача 34. Решение
+
+Console.WriteLine("Задача 34. Решение:");
+
+int lengthArray = ReadNumber("Введите длину массива: ");
+Console.WriteLine("Длина массива = " + lengthArray);
+int[] array = CreateArray(lengthArray, 100, 999);
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine("Количество четных чисел в массиве = " + FindEvenNumbersInArray(array));
+
+// Задача 36. Решение
+Console.WriteLine("Задача 36. Решение:");
+int lengthArray1 = ReadNumber("Введите длину массива: ");
+Console.WriteLine("Длина массива = " + lengthArray1);
+int[] array1 = CreateArray(lengthArray1, -100, 100);
+Console.WriteLine(string.Join(", ", array1));
+Console.WriteLine("Сумма элементов, стоящих на нечётных позициях = " + GetSummElementsInOddPositions(array1));
+
+// Задача 38. Решение
+Console.WriteLine("Задача 38. Решение:");
+int lengthArray2 = ReadNumber("Введите длину массива: ");
+Console.WriteLine("Длина массива = " + lengthArray2);
+int[] array2 = CreateArray(lengthArray2, -100, 100);
+Console.WriteLine(string.Join(", ", array2));
+
+// 1 Способ
+Console.WriteLine("2 Способ. Решение:");
+Console.WriteLine("Разница между максимальным и минимальным элементов массива = " + GetDiffMaxMinElementsArray(array2));
+
+// 2 Способ
+Console.WriteLine("2 Способ. Решение:");
+int minElArray;
+int maxElArray;
+GetMaxMinElementsArray(array2, out minElArray, out maxElArray);
+Console.WriteLine("Разница между максимальным и минимальным элементов массива = " + (maxElArray - minElArray));
+
+
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+int ReadNumber(string text)
+{
+    Console.WriteLine(text);
+    int number;
+    string readNumber = Console.ReadLine() ?? "Nullable";
+    if (int.TryParse(readNumber, out number)) { }
+    else
+    {
+        Console.WriteLine("Ввод некорректный. Вводите только цифры");
+        number = ReadNumber(text);
+    }
+    return number;
+}
+
+int FindEvenNumbersInArray(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int GetSummElementsInOddPositions(int[] array)
+{
+    int summ = 0;
+    for(int i = 0; i < array.Length; i+=2)
+    {
+        summ += array[i];
+    }
+    return summ;
+}
+
+int GetDiffMaxMinElementsArray(int[] array)
+{
+    int min = array[0];
+    int max = array[0];
+    for(int i = 1; i < array.Length; i++)
+    {
+        if(array[i] < min)
+        {
+            min = array[i];
+        }
+        if(array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    Console.WriteLine($"max={max}, min={min}");
+    return max - min;
+}
+
+bool GetMaxMinElementsArray(int[] array, out int min, out int max)
+{
+    min = array[0];
+    max = array[0];
+    for(int i = 1; i < array.Length; i++)
+    {
+        if(array[i] < min)
+        {
+            min = array[i];
+        }
+        if(array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    Console.WriteLine($"max={max}, min={min}");
+    return true;
+}
+
---
+++ Seminar005/Program.cs
@@ -0,0 +1,129 @@
+﻿using System.Reflection;
+// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+
+// int[] array = new int[] { 1, -2, 3, -4, 5, -6, 7, -8 };
+
+// //Задача 33
+// Console.WriteLine(SearchNumberInArray(12, array));
+// Console.WriteLine(SearchNumberInArray(1, array));
+
+
+// //Задача 32
+
+// Console.WriteLine(string.Join(",", ChangeArray(array)));
+
+// int[] array1 = CreateArray(123);
+// Console.WriteLine(string.Join(",", array1));
+// Console.WriteLine(SearchCountNumberInArray(array1, 10, 99));
+
+
+
+
+
+// int[] ChangeArray(int[] array)
+// {
+//     int[] newArray = new int[array.Length];
+
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         newArray[i] = array[i] * -1;
+//     }
+//     return newArray;
+// }
+
+
+// string SearchNumberInArray(int number, int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] == number)
+//         {
+//             return "да";
+//         }
+//     }
+//     return "нет";
+// }
+
+// // Задача 35
+int[] CreateArray(int size) 
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++) 
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    return array;
+}
+
+// int SearchCountNumberInArray(int[] array, int min, int max)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] >= min && array[i] <= max) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве. [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] array2 = CreateArray(5);
+Console.WriteLine(string.Join(",", array2));
+
+int[] pairs = GetPairsInArray(array2);
+Console.WriteLine(string.Join(",", pairs));
+
+
+int[] GetPairsInArray (int[] array) 
+{
+    int lengthPairs; //длина массива "произведение пар"
+    bool count;
+    // Поиск этой длины
+    if (array.Length % 2 == 0)
+    {
+        lengthPairs = array.Length / 2;
+        count = true;
+    } else{
+        lengthPairs = array.Length / 2 + 1;
+        count = false;
+    }
+
+    int[] pairs = new int[lengthPairs];
+
+    for (int i =0; i < pairs.Length - 1; i++) 
+    {
+        pairs[i] = array[i] * array[array.Length - 1 - i];
+    }
+
+    if(count)
+    {
+        pairs[pairs.Length - 1] = array[array.Length / 2] * array[array.Length/2 - 1];
+    } else {
+        pairs[pairs.Length - 1] = array[array.Length / 2];
+    }
+
+    return pairs;
+}
+
+
+
+
