bf3e21102009f62cab904f3e4d727bc8faa22037
Timur Guliev <mail@guliev-timur.ru>
2022-12-26 00:38:27 +0500

    add HomeTask009

---
+++ HomeTask009/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// Задача 64.
+Console.WriteLine("\n\nЗадача 64");
+Console.WriteLine("Введите N");
+int number = int.Parse(Console.ReadLine());
+PrintNumber(number);
+void PrintNumber(int i)
+{
+    if (i == 1)
+    {
+        Console.WriteLine(i);
+        return;
+    }
+    Console.Write($"{i}, ");
+    i--;
+    PrintNumber(i);
+
+}
+
+// Задача 66
+Console.WriteLine("\n\nЗадача 66");
+Console.WriteLine("Введите N");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите M");
+int m = int.Parse(Console.ReadLine());
+int result = 0;
+Console.WriteLine(Sum(n, m));
+
+int Sum(int n, int m)
+{
+    if (m <= n)
+        return n;
+    return m + Sum(n, m - 1);
+}
+
+// Задача 68
+Console.WriteLine("\n\nЗадача 68");
+Console.WriteLine("Вычисление функции Аккермана");
+Console.WriteLine("Введите N");
+int j = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите M");
+int k = int.Parse(Console.ReadLine());
+Console.WriteLine(Akkerman(j,k));
+
+int Akkerman(int n, int m)
+{
+    if (n == 0)
+    {
+       return m + 1; 
+    } else
+    {
+        if ((n != 0) && (m == 0))
+        {
+            return Akkerman(n - 1, 1);
+        }else
+        {
+           return Akkerman(n - 1, Akkerman(n, m - 1)); 
+        }
+    }
+}
---
+++ Seminar009/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5" M = 4; N = 8 -> "4, 6, 7, 8"
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12 45 -> 9
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (35) A = 2; B = 3 -> 8
+
+
+// Задача 63.
+Console.WriteLine("Введите N");
+int t = int.Parse(Console.ReadLine());
+PrintNumber(100);
+void PrintNumber( int i)
+{
+    if(i == t)
+    {
+        return;
+    }
+    Console.WriteLine(i);
+    i++;
+    PrintNumber(i);
+
+}
+
+// Задача 65.
+Console.WriteLine("Введите N");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите M");
+int m = int.Parse(Console.ReadLine());
+
+void PrintNumberMN(int n, int m)
+{
+    if(i == n)
+    {
+        return;
+    }
+    Console.WriteLine(m);
+    m++;
+    PrintNumberMN(n, m);
+}
+
+
+// Задача 67
+Console.WriteLine("Введите N");
+int number = int.Parse(Console.ReadLine());
+int sum = 0; 
+Console.WriteLine(GetSumNumber(number));
+
+int GetSumNumber(int number)
+{
+    if(number < 1)
+    {
+        return sum;
+    }
+    sum = sum + number % 10;       
+    return GetSumNumber(number / 10);
+}
+
+// Задача 69:
+Console.WriteLine("Введите A");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите B");
+int b = int.Parse(Console.ReadLine());
+int result = 1;
+Console.WriteLine(GetExponentAB(a,b));
+
+int GetExponentAB(int a, int b)
+{
+    if(b == 0)
+    {
+        return result;
+    }
+
+    result = result * a;
+    b--;
+
+    return GetExponentAB(a, b);
+
+}
